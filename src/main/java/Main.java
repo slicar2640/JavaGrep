d50ce8fcf8916cda8be650da8d3a9f5673e50130
theslinkers2011 <theslinkers2011@gmail.com>
2025-08-27 21:08:01 -0500

    Recursive search

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,20 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 import grep.Pattern;
 import grep.PatternCompiler;
 
 public class Main {
   public static void main(String[] args) {
-    String patternString = args[1];
-    Pattern pattern = new PatternCompiler(patternString).compile();
     if (args.length == 2) {
+      Pattern pattern = new PatternCompiler(args[1]).compile();
       Scanner scanner = new Scanner(System.in);
       String inputLine = scanner.nextLine();
 
@@ -19,31 +25,81 @@ public class Main {
       }
       scanner.close();
     } else {
-      boolean foundOne = false;
-      for (int i = 2; i < args.length; i++) {
-        String filepath = args[i];
-        try (Scanner scanner = new Scanner(new File(filepath))) {
-          while (scanner.hasNextLine()) {
-            String inputLine = scanner.nextLine();
-            if (pattern.matches(inputLine)) {
-              foundOne = true;
-              if (args.length > 3) {
-                System.out.println(filepath + ":" + inputLine);
-              } else {
-                System.out.println(inputLine);
-              }
+      String firstFlag = args[0];
+      if (firstFlag.equals("-E")) {
+        Pattern pattern = new PatternCompiler(args[1]).compile();
+        searchTextFiles(pattern, Arrays.copyOfRange(args, 2, args.length));
+      } else if (firstFlag.equals("-r")) {
+        Pattern pattern = new PatternCompiler(args[2]).compile(); // skip -E
+        handleRecursive(pattern, args[3]); // skip -E
+      }
+    }
+  }
+
+  private static void searchTextFiles(Pattern pattern, String... filepaths) {
+    boolean foundOne = false;
+    for (int i = 0; i < filepaths.length; i++) {
+      String filepath = filepaths[i];
+      try (Scanner scanner = new Scanner(new File(filepath))) {
+        while (scanner.hasNextLine()) {
+          String inputLine = scanner.nextLine();
+          if (pattern.matches(inputLine)) {
+            foundOne = true;
+            if (filepaths.length > 1) {
+              System.out.println(filepath + ":" + inputLine);
+            } else {
+              System.out.println(inputLine);
             }
           }
-        } catch (Exception e) {
-          e.printStackTrace();
-          System.exit(1);
         }
+      } catch (Exception e) {
+        e.printStackTrace();
+        System.exit(2);
       }
-      if (foundOne) {
-        System.exit(0);
-      } else {
-        System.exit(1);
+    }
+    if (foundOne) {
+      System.exit(0);
+    } else {
+      System.exit(1);
+    }
+  }
+
+  private static void handleRecursive(Pattern pattern, String filepath) {
+    Path startPath = Paths.get(filepath);
+    if (!Files.exists(startPath)) {
+      System.err.println("Error: path does not exist: " + filepath);
+      System.exit(2);
+    }
+    boolean foundOne = false;
+    if (Files.isDirectory(startPath)) {
+      try (Stream<Path> stream = Files.walk(startPath)) {
+        Iterator<Path> iterator = stream.filter(Files::isRegularFile).iterator();
+        while (iterator.hasNext()) {
+          Path currentPath = iterator.next();
+          try (Scanner scanner = new Scanner(currentPath)) {
+            while (scanner.hasNextLine()) {
+              String inputLine = scanner.nextLine();
+              if (pattern.matches(inputLine)) {
+                foundOne = true;
+                System.out.println(currentPath.toString() + ":" + inputLine);
+              }
+            }
+          } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(2);
+          }
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+        System.exit(2);
       }
+    } else {
+      searchTextFiles(pattern, filepath);
+    }
+    if (foundOne) {
+      System.exit(0);
+    } else {
+      System.exit(1);
     }
   }
 }
